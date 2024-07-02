fa9da7b434bf562bba97e9856fb0bdf940c0c0d7
Artyom <a-polyanskiy@inbox.ru>
2024-07-03 01:26:29 +0300

    first

---
+++ Probel/Probel/Probel.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <math.h>
+#include<conio.h>
+
+double calculate_S(double A, double B) {
+    double result;
+
+    result = log(sqrt(A) * pow(B, 2));
+    
+    return result;
+}
+
+int main()
+{
+
+    double A, B, S;
+
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+
+    S = calculate_S(A, B);
+
+    _cprintf_s("%lf", S);
+
+    return 0;
+}
---
+++ Probel2/Probel2/Probel2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+double calculate_S(double S) {
+    double result = pow(S, 0.25);
+    return result;
+}
+
+int main()
+{
+    double S;
+    scanf("%lf", &S);
+    double result = calculate_S(S);
+    _cprintf_s("%.2lf", result);
+    return 0;
+}
